9c47629d00204404f7fb4f82c436263ce542461b
jeffryendrianawijaya <95774894+jeflijonathan@users.noreply.github.com>
2026-05-20 01:00:15 +0700

    add

--- cepu_app/lib/screens/add_post_screen.dart
+++ cepu_app/lib/screens/add_post_screen.dart
@@ -204,6 +204,37 @@ class _AddPostScreenState extends State<AddPostScreen> {
     );
   }
 
+  Future<void> sendNotificationToTopic(String body, String senderName) async {
+    final url = Uri.parse('https://fasum-cloud-pipx.vercel.app/send-to-topic');
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        "topic": "berita-fasum",
+        "title": "🚨 Laporan Baru",
+        "body": body,
+        "senderName": senderName,
+        "senderPhotoUrl": "https://static.vecteezy.com/system/resources/thumbnails/041/642/167/small_2x/ai-generated-portrait-of-handsome-smiling-young-man-with-folded-arms-isolated-free-png.png",
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('✓ Notifikasi berhasil dikirim')),
+        );
+      }
+    } else {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('✕ Gagal kirim notifikasi: ${response.body}')),
+        );
+      }
+    }
+  }
+
   Future<void> _submitPost() async {
     if (_base64Image == null) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -254,6 +285,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
       );
 
       if (mounted) {
+        sendNotificationToTopic(_descriptionController.text.trim(), fullName ?? 'Anonymous');
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text("Laporan berhasil dikirim!")),
         );
